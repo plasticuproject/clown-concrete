b3d4cdc97dc8ef0dae1d5b9ee5e0cab8277903c0
plasticuproject <19690649+plasticuproject@users.noreply.github.com>
2024-09-04 14:02:18 -0400

    Odds and ends on scripts

--- tear_down.sh
+++ tear_down.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+docker stop clown-concrete-nginx-proxy-1 clown-concrete-flask-app-1 clown-concrete-pypiserver-authenticated-1
 docker rm clown-concrete-nginx-proxy-1 clown-concrete-flask-app-1 clown-concrete-pypiserver-authenticated-1
 docker rmi clown-concrete-nginx-proxy clown-concrete-flask-app pypiserver/pypiserver:v1.5.1
+docker volume prune
